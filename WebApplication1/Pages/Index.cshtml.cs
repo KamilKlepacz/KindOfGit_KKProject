16461ee9f7948751d76bd066f3b8c5201fceea5b
KamilK <kamilnor01@gmail.com>
2024-09-09 17:34:34 +0200

    More files

--- WebApplication1/Pages/Index.cshtml.cs
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using LibGit2Sharp;
 
 namespace WebApplication1.Pages
 {
@@ -12,13 +13,24 @@ namespace WebApplication1.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
+        public List<Commit> GetCommit()
+        {
+            using (var repo = new Repository("C:\\Users\\kamil\\RiderProjects\\test_project"))
+            {
+                return repo.Commits.Take(10).ToList();
+            }
+        }
+
+
         public void OnGet()
         {
+
         }
     }
 }
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using LibGit2Sharp;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
@@ -7,15 +9,52 @@ namespace WebApplication1
 {
     public class Program
     {
+        public List<Commit> getCommit()
+        {
+            using (var repo = new Repository("C:\\Users\\kamil\\RiderProjects\\test_project"))
+            {
+                return repo.Commits.Take(10).ToList();
+            }
+        }
+
         public static void Main(string[] args)
         {
-           // CreateHostBuilder(args).Build().Run();
-            using (var repo = new Repository("C:\\Users\\kamil\\RiderProjects\\WebApplication1\\"))
+            CreateHostBuilder(args).Build().Run();
+            using (var repo = new Repository("C:\\Users\\kamil\\RiderProjects\\test_project"))
             {
-                foreach (var item in repo.RetrieveStatus(new LibGit2Sharp.StatusOptions()))
+                
+                var branch = repo.Branches["master"];
+                
+                if (branch == null)
                 {
-                    Console.WriteLine(item.FilePath);
+                    // repository return null object when branch not exists
+                    
                 }
+                
+                Branch currentBranch = Commands.Checkout(repo, branch);
+
+                
+                List<Commit> commitList = repo.Commits.Take(10).ToList();
+                List<Branch> branchList = repo.Branches.ToList();
+                Console.WriteLine(commitList.ToString());
+                Console.WriteLine(branchList.ToString());
+                // foreach (Commit c in repo.Commits.Take(15))
+                // {
+                //     Console.WriteLine(string.Format("commit {0}", c.Id));
+                //
+                //     if (c.Parents.Count() > 1)
+                //     {
+                //         Console.WriteLine("Merge: {0}",
+                //             string.Join(" ", c.Parents.Select(p => p.Id.Sha.Substring(0, 7)).ToArray()));
+                //     }
+                //
+                //     Console.WriteLine(string.Format("Author: {0} <{1}>", c.Author.Name, c.Author.Email));
+                //     Console.WriteLine();
+                //     Console.WriteLine(c.Message);
+                //     Console.WriteLine();
+                // }
+
+
             }
         }
 
