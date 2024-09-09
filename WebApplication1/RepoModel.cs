652db40617f4b290cf3bc25defee5c3e8898e0d4
KamilK <kamilnor01@gmail.com>
2024-09-09 17:47:40 +0200

    Added repo Models, - cleaning up

---
+++ WebApplication1/RepoModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using LibGit2Sharp;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
+
+
+namespace WebApplication1
+{
+    public class RepoModel
+    {
+        public List<Commit> getCommit()
+        {
+            using (var repo = new Repository("C:\\Users\\kamil\\RiderProjects\\test_project"))
+            {
+                return repo.Commits.Take(10).ToList();
+            }
+        }
+
+        public List<string> getMessages()
+        {
+            using (var repo = new Repository("C:\\Users\\kamil\\RiderProjects\\test_project"))
+            {
+                return repo.Commits.Select(x => x.Message).ToList();
+            }
+        }
+        public List<string> GetFiles(String branch)
+        {
+            using (var repo = new Repository("C:\\Users\\kamil\\RiderProjects\\test_project"))
+            {
+                List<string> files = new List<string>();
+                Branch currentBranch = Commands.Checkout(repo, branch);
+                foreach(IndexEntry e in repo.Index)
+                {
+                   files.Add(e.Path);
+                }
+                return files;
+            }
+                
+        }
+        public List<Branch> GetBranches()
+        {
+            using (var repo = new Repository("C:\\Users\\kamil\\RiderProjects\\test_project"))
+            {
+                return repo.Branches.Take(10).ToList();
+            }
+        }
+    }
+}
